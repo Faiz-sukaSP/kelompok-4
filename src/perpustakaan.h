be7c3e403017f7bebd1ac92b77c7f851fde58263
Khoirotun Hisan Br.B <khoirotunhisanbrb@gmail.com>
2025-11-16 16:32:41 +0700

    Update perpustakaan.h
    tambah struct dan prototype utility

--- src/perpustakaan.h
+++ src/perpustakaan.h
@@ -36,6 +36,16 @@ typedef enum{
     TERSEDIA = 1,
     DIPINJAM
 } Status;
+
+// Kategori Buku
+typedef enum { 
+    FIKSI, NON_FIKSI, KOMIK, MAJALAH, REFERENSI 
+} Kategori;
+
+// Status Buku
+typedef enum { 
+    TERSEDIA, DIPINJAM 
+} Status;
                                         //=== STRUCK ===
 // Struck untuk data buku
 typedef struct{
@@ -75,7 +85,7 @@ typedef struct {
 } pustakawan;
                                         // === PROTOTYPE FUNGSI === //
 
-// PF PEMINJAMAN //
+// === PF PEMINJAMAN === //
 int generateIDPeminjaman();
 int uraiTanggal(const char *tgl)
 double hitungDenda(const char *tglPinjam, const char *tglKembali);
@@ -105,5 +115,21 @@ void cariBukuByKategori(Kategori kategori);
 int cariIndexBuku(const char *id_buku);
 void urutkanBukuBerdasarkanPeminjaman(Buku *data, int jumlah);
 void tampilkanRankingBuku();
+
+// === PF UTILITY ===//
+void clear_input_buffer(void);
+void pause_screen(void);
+const char* kategori_string(Kategori k);
+const char* status_string(Status s);
+
+// Tanggal
+void get_tanggal_sekarang(char* buffer);
+void tambah_hari(const char* tanggal, int hari, char* hasil);
+int hitung_selisih_hari(const char* day1, const char* day2);
+
+// Pencarian data
+int cari_index_buku(const char* id);
+int cari_index_anggota(const char* id);
+int hitung_pinjaman_aktif(const char* id_anggota);
 
 #endif
